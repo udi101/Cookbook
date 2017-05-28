7d083e04ebb6d0eedae7ef07a9511f9789aa06ab
udi101 <udimaz@gmail.com>
2017-05-28 15:05:15 +0300

    28.5

---
+++ src/app/Family/Grandfather/grandfather.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-grandfather',
+    templateUrl: './grandfather.component.html',
+    styleUrls: ['./grandfather.component.css']
+})
+export class GrandfatherComponent {
+
+}
---
+++ src/app/Family/family.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+// Components
+
+@NgModule({
+    imports: [CommonModule],
+    exports: [],
+    declarations: [],
+    providers: []
+})
+export class FamilyModule {
+}
